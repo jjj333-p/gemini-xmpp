0b5974a38cb11dfd82d29851e10cfcf4c8ff93ec
jjj333_p <jjj333.p.1325@gmail.com>
2025-07-27 19:50:31 -1000

    print("Connected and running forever...")

--- main.py
+++ main.py
@@ -307,4 +307,5 @@ if __name__ == '__main__':
 
     # Connect to the XMPP server and start processing XMPP stanzas.
     xmpp.connect()
+    print("Connected and running forever...")
     asyncio.get_event_loop().run_forever()
